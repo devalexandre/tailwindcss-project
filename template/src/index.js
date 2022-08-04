071e6914c6e375cf19917e2aa96cbf655e392873
DevEvangelista <alexandre@indev.net.br>
2022-08-04 20:09:34 -0300

    feat: add tailwind-material

--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,10 @@
 import './styles/index.css';
 import { h } from 'preact';
+import { ThemeProvider , Button } from "@material-tailwind/react";
+
 
 const App = () =>
+<ThemeProvider>
     <div class="template">
         <header class="header">
          <h1>Template Tailwind for Preact</h1>
@@ -24,10 +27,11 @@ const App = () =>
                         <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
                         </svg>
                 </span>
-               <span>Alexandre E Souza</span>
+               <Button>Alexandre E Souza</Button>
                </div>
         </footer>
     </div>
+    </ThemeProvider>
 
 
 export default App;
